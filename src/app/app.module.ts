00cd706ea4f19f5b43baeee3619a0fee0a2459ff
jaorsa <A01329978@itesm.mx>
2021-03-10 00:09:38 -0600

    Componentes generados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NominasComponent } from './nominas/nominas.component';
+import { SupervisorComponent } from './supervisor/supervisor.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
+import { DocumentacionComponent } from './documentacion/documentacion.component';
+import { RecursosHumanosComponent } from './recursos-humanos/recursos-humanos.component';
+import { ContabilidadComponent } from './contabilidad/contabilidad.component';
+import { PortalComponent } from './portal/portal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NominasComponent,
+    SupervisorComponent,
+    MenuPrincipalComponent,
+    DocumentacionComponent,
+    RecursosHumanosComponent,
+    ContabilidadComponent,
+    PortalComponent
   ],
   imports: [
     BrowserModule,
