af45aa7cf829be1ee9ff0ed2ad1b2165971da2b1
jaorsa <A01329978@itesm.mx>
2021-03-10 06:13:21 -0600

    Recursos Humanos listo

--- src/app/recursos-humanos/recursos-humanos.component.ts
+++ src/app/recursos-humanos/recursos-humanos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
+import {FormControl, Validators} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { CrudService } from '../../services/crud.service';
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-recursos-humanos',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recursos-humanos.component.css']
 })
 export class RecursosHumanosComponent implements OnInit {
+  date = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  document = {
+    name: "Prueba file.ext",
+    ext: ".ext",
+    fecha: this.date, 
+    periodo: "2020-12-01", 
+    estado: 0,
+    isActive: false, 
+    usuario_fk: 1,
+    dept:"100"
+  }
 
-  constructor() { }
+  user = {
+    id: 1,
+    dept: "001"
+  }
+
+  constructor(private router: Router, 
+    private crudService: CrudService,
+    private auth: AuthService) { }
 
   ngOnInit(): void {
+    this.crudService.getDocs_recursosHumanos(this.user)
+      .then(res => {
+        this.document = res.data;
+        console.log("si se pudo");
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
 }
