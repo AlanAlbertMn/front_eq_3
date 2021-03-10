3e89784644c4175c14046dd5688ac8b56e11cd73
jaorsa <A01329978@itesm.mx>
2021-03-10 03:57:27 -0600

    crud cambios

--- src/services/crud.service.ts
+++ src/services/crud.service.ts
@@ -18,55 +18,60 @@ export class CrudService {
     }
 
   getdocs_admin(){
-    let url = this.URL + '/getdocsadmin/';
+    let url = this.URL + 'getdocsadmin/';
     return axios.get(url, {
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   get_doc(body){
-    let url = this.URL + '/adddocument/';
+    let url = this.URL + 'adddocument/';
     console.log("pasame el pack papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   get_notifications(){
-    let url = this.URL + '/getnotif/';
+    let url = this.URL + 'getnotif/';
     return axios.get(url, {
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   add_document(body){
-    let url = this.URL + '/adddocument/';
+    let url = this.URL + 'adddocument/';
     console.log(body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   approve_doc(body){
-    let url = this.URL + '/approvedoc/';
+    let url = this.URL + 'approvedoc/';
     console.log("aqui tienes el body papu " + body);
     return axios.put(url, body, {headers:{'Content-Type': 'application/json'}});
   }
   
   delete_doc(value){
-    let url = this.URL + '/deletedoc/';
+    let url = this.URL + 'deletedoc/';
     console.log("borra este doc papu " + value);
     let config = { 
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
       body: { //! Take note of the `data` keyword. This is the request body.
           id: value
@@ -76,51 +81,56 @@ export class CrudService {
   }
 
   getdocsByUser(body){
-    let url = this.URL + '/adddocument/';
+    let url = this.URL + 'adddocument/';
     console.log("aqui te van los docs papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   create_notification(body){
-    let url = this.URL + '/addnotif/';
+    let url = this.URL + 'addnotif/';
     console.log("aqui te va un anuncio papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   get_nomina(body){
-    let url = this.URL + '/getnomina/';
+    let url = this.URL + 'getnomina/';
     console.log("aqui te va un notificacion papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   getDocs_contabilidad(body){
-    let url = this.URL + '/getcontabilidad/';
+    let url = this.URL + 'getcontabilidad/';
     console.log("aqui te van los docs papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
 
   getDocs_recursosHumanos(body){
-    let url = this.URL + '/getrh/';
+    let url = this.URL + 'getrh/';
     console.log("aqui te van los docs papu" + body);
     return axios.post(url,body,{
       headers:{
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       }
     });
   }
