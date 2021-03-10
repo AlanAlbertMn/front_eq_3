b9d5440bcb61fa46b4562db762d2de702f7073e2
tspt89 <tspt89@hotmail.com>
2021-03-10 05:31:33 -0600

    Hasta nomina-year component HTML

---
+++ src/app/nomina-year/nomina-year.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nomina-year',
+  templateUrl: './nomina-year.component.html',
+  styleUrls: ['./nomina-year.component.css']
+})
+export class NominaYearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
--- src/app/nominas/nominas.component.ts
+++ src/app/nominas/nominas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-nominas',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NominasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  goNominaYear(){
+    this.router.navigate(['nominayear'])
+  } 
 
 }
